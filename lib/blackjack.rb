4add013c498830092c269e949b6b0dc3f86bd9c9
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-30 18:52:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(card_total)
   input = get_user_input
   if input == "s"
     card_total
-  elseif input == "h"
+  elsif input == "h"
     card_total += deal_card
   else
     invalid_command
